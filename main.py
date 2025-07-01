8119a7ebd541cf670fa3a0f3eba8d4fd8f94a5d4
Matthew Stefanovic <matthew@stefanovic.us>
2025-07-01 03:11:42 +0000

    Got script working

--- main.py
+++ main.py
@@ -3,25 +3,57 @@ import zoho
 import mxroute
 
 def main():
-    client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
-    org_id = os.getenv('ORGANIZATION_ID')
+  # Zoho Vars
+  client_id = os.getenv('CLIENT_ID')
+  client_secret = os.getenv('CLIENT_SECRET')
+  org_id = os.getenv('ORGANIZATION_ID')
 
-    zoho_client = zoho.Zoho(client_id, client_secret, org_id)
-    user_emails = zoho_client.get_user_emails()
-    group_emails = zoho_client.get_group_emails()
-    zoho_emails = user_emails | group_emails
+  # MXRoute Vars
+  user_id = os.getenv('USER_ID')
+  password = os.getenv('PASSWORD')
+  server = os.getenv('SERVER')
 
-    user_id = os.getenv('USER_ID')
-    password = os.getenv('PASSWORD')
-    server = os.getenv('SERVER')
+  # Getting zoho users
+  # NOTE: As of now there is not a way to get shared inboxes via
+  # the api. Those will have to be manually entered.
+  zoho_client = zoho.Zoho(client_id, client_secret, org_id)
+  user_emails = zoho_client.get_user_emails()
+  group_emails = zoho_client.get_group_emails()
+  zoho_emails = user_emails | group_emails
 
-    mxroute_client = mxroute.MXroute(user_id, password, server)
-    domains = mxroute_client.list_domains()
-    forwarders = set()
-    for domain in domains:
-        forwarders = forwarders | mxroute_client.list_forwarders(domain)
-    print(forwarders)
+  # Getting forwarders from mxroute
+  mxroute_client = mxroute.MXroute(user_id, password, server)
+  domains = mxroute_client.list_domains()
+
+  forwarders = set()
+  for domain in domains:
+    forwarders = forwarders | mxroute_client.list_forwarders(domain)
+
+  print(forwarders)
+
+  # Create expected forwarder destinations
+  expected_forwarders = set()
+  for zoho_email in zoho_emails:
+    split = zoho_email.split('@')
+    user = split[0]
+    domain = split[1]
+    dest = f'{user}@zoho.{domain}'
+    expected_forwarders.add(dest)
+
+    # Add missing forwarders
+    if dest not in forwarders:
+      print(f'Adding forwarder {user}@{domain} --> {dest} to MXRoute')
+      mxroute_client.add_forwarder(domain, user, dest)
+
+  # Remove forwarders that are no longer needed
+  for forwarder in forwarders:
+    if '@zoho.' in forwarder and forwarder not in expected_forwarders:
+      # Extract the original domain and user from the forwarder destination
+      parts = forwarder.replace('@zoho.', '@').split('@')
+      original_user = parts[0]
+      original_domain = parts[1]
+      print(f'Removing forwarder {original_user}@{original_domain} --> {forwarder} from MXRoute')
+      mxroute_client.delete_forwarder(original_domain, original_user)
 
 if __name__ == '__main__':
-    main()
+  main()
--- mxroute.py
+++ mxroute.py
@@ -1,30 +1,51 @@
 import requests
 
 class MXroute:
-    def __init__(self, username, password, server):
-        self.username = username
-        self.password = password
-        self.server = server
+  def __init__(self, username, password, server):
+    self.username = username
+    self.password = password
+    self.server = server
 
-    def make_call(self, function):
-        pass
+  def list_domains(self):
+    url = f'{self.server}//CMD_API_SHOW_DOMAINS?json=yes'
+    resp = requests.get(url, auth=(self.username, self.password))
 
-    def list_domains(self):
-        url = f'{self.server}//CMD_API_SHOW_DOMAINS?json=yes'
-        resp = requests.get(url, auth=(self.username, self.password))
+    return resp.json()
 
-        return resp.json()
+  def list_forwarders(self, email_domain):
+    url = f'{self.server}/CMD_API_EMAIL_FORWARDERS?domain={email_domain}&json=yes'
+    resp = requests.get(url, auth=(self.username, self.password))
 
-    def list_forwarders(self, email_domain):
-        url = f'{self.server}/CMD_API_EMAIL_FORWARDERS?domain={email_domain}&json=yes'
-        resp = requests.get(url, auth=(self.username, self.password))
+    emails = set()
+    for value in resp.json().values():
+      for email in value:
+        emails.add(email)
 
-        emails = set()
-        for value in resp.json().values():
-            for email in value:
-                emails.add(email)
+    return emails
 
-        return emails
+  def add_forwarder(self, domain, user, email):
+    url = f'{self.server}/CMD_EMAIL_FORWARDER?json=yes'
+    headers = {'Content-Type': 'application/json'}
+    payload = {
+      'action': 'create',
+      'domain': domain,
+      'user': user,
+      'email': email,
+      'create': 'Create'
+    }
+    resp = requests.post(url, auth=(self.username, self.password), 
+              headers=headers, json=payload)
+    return resp.json() if resp.status_code == 200 else resp.text
 
-    def delete_forwarder(self, email_from, email_to):
-        pass
+  def delete_forwarder(self, domain, user):
+    url = f'{self.server}/CMD_EMAIL_FORWARDER?json=yes'
+    headers = {'Content-Type': 'application/json'}
+    payload = {
+      'action': 'delete',
+      'domain': domain,
+      'user': user,
+      'delete': 'Delete'
+    }
+    resp = requests.post(url, auth=(self.username, self.password), 
+              headers=headers, json=payload)
+    return resp.json() if resp.status_code == 200 else resp.text
